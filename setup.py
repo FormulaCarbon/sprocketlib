3bf8b4d35c2acdb3fb078b286a2c3a0056c2a547
FormulaCarbon <siddhuk.learning@gmail.com>
2024-07-03 16:05:05 -0500

    fix??

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='sprocketlib',
     packages=find_packages(include=['sprocketlib']),
-    version='0.1.0',
+    version='0.1.1',
     description='Sprocket Blueprint Loading Library',
     author='FormulaCarbon',
     install_requires=[],
